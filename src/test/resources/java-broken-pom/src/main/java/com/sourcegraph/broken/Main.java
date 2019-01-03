e5a10ec45dd9e845070fac0e23c0885513c1b714
Beyang Liu <beyang@sourcegraph.com>
2019-01-02 16:39:55 -0800

    remove empty integration test directories

--- src/test/resources/java-broken-pom/src/main/java/com/sourcegraph/broken/Main.java
+++
@@ -1,6 +0,0 @@
-package com.sourcegraph.broken;
-
-public class Main {
-    public static void main(String... args) throws Exception {
-    }
-}
--- src/test/resources/java-pom-with-system-dependencies/src/main/java/com/sourcegraph/simple/Main.java
+++
@@ -1,9 +0,0 @@
-package com.sourcegraph.simple;
-
-import org.apache.commons.io.IOUtils;
-
-public class Main {
-    public static void main(String... args) throws Exception {
-        char c = IOUtils.DIR_SEPARATOR;
-    }
-}
