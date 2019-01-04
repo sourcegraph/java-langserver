834c51f75813695f6347b4a1c9f255306330dc89
Beyang Liu <beyang@sourcegraph.com>
2019-01-04 11:32:13 -0800

    wip

--- src/main/java/com/sourcegraph/langserver/WebSocketStreamConnection.java
+++ src/main/java/com/sourcegraph/langserver/WebSocketStreamConnection.java
@@ -1,74 +1,81 @@
 package com.sourcegraph.langserver;
 
+import com.sourcegraph.lsp.domain.Mapper;
+import com.sourcegraph.lsp.jsonrpc.Reader;
 import com.sun.tools.internal.ws.wsdl.document.Output;
 import org.java_websocket.WebSocket;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * WebSocketStreamConnection presents a stream-based interface for reading/writing on a WebSocket connection.
  * It uses JSON-RPC messages to determine the frame boundaries.
  */
 public class WebSocketStreamConnection {
+
+    private static ExecutorService executorService = new ThreadPoolExecutor(
+            2,
+            5,
+            60,
+            TimeUnit.SECONDS,
+            new LinkedBlockingQueue<>()
+    );
+
     WebSocket conn;
-    InputStream in;
-    OutputStream out;
+
+    /**
+     * Output pipe
+     */
+    PipedOutputStream outPipeOut;
+    PipedInputStream outPipeIn;
+
+    /**
+     * Input pipe
+     */
+    PipedOutputStream inPipeOut;
+    PipedInputStream inPipeIn;
+
+
     public WebSocketStreamConnection(WebSocket conn) {
         this.conn = conn;
-        this.in = new InputStream(conn);
-        this.out = new OutputStream(conn);
+
+        try {
+            outPipeIn = new PipedInputStream(100000);
+            outPipeOut = new PipedOutputStream(outPipeIn);
+            Reader reader = new Reader(outPipeIn);
+            reader.getInputObservable().subscribe(conn::send);
+            reader.startReading();
+
+            inPipeIn = new PipedInputStream(100000);
+            inPipeOut = new PipedOutputStream(inPipeIn);
+        } catch (IOException e) {
+            throw new RuntimeException(e); // TODO(beyang)
+        }
+
     }
 
-    public InputStream input() {
-        return in;
+    public InputStream in() {
+        return inPipeIn;
     }
 
     public OutputStream out() {
-        return out;
+        return outPipeOut;
     }
+
+    // NEXT: hook up receive to WebSocketServer; then plug into lsp4j Launcher
 
     /**
      * receive receives incoming messages and writes these to the input stream
      */
     public void receive(String message) {
-        // NEXT: add input/output stream buffers
-    }
-
-    /**
-     * writeNext writes the next sequence of bytes that comprises a complete WebSocket message by reading
-     * from the output buffer.
-     */
-    void writeNext(String message) {
-
-    }
-
-    private static class OutputStream extends java.io.OutputStream {
-        WebSocket conn;
-        OutputStream(WebSocket conn) {
-            this.conn = conn;
-        }
-        @Override
-        public void write(int b) throws IOException {
-            // TODO
-        }
-    }
-
-    private static class InputStream extends java.io.InputStream {
-        WebSocket conn;
-        InputStream(WebSocket conn) {
-            this.conn = conn;
-        }
-
-        void receive(String message) {
-
-        }
-
-        @Override
-        public int read() throws IOException {
-            // TODO
-            return 0;
+        try {
+            inPipeOut.write(message.getBytes());
+        } catch (Exception e) {
+            throw new RuntimeException(e); // TODO(beyang): replace with error
         }
     }
 }
